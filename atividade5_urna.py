dd7fa009754e2e3a34f290009df0719b51ae4391
Luv <81397423+Luvlore@users.noreply.github.com>
2021-08-19 21:29:14 -0300

    Update atividade5_urna.py

--- atividade5_urna.py
+++ atividade5_urna.py
@@ -1,24 +1,34 @@
-print ('URNA ELETRONICA')
+def centralizado(texto):
+    print(f'{texto:^38}')
+
+def risquinhos():
+    print('—'*38)
+
 candidatos = ['João', 'José', 'Jacó', 'Jobé', 'Nulo', 'Branco']
 votos = [0, 0, 0, 0, 0, 0]
-numeros = 1
 
-for candidato in candidatos:
-    print(numeros, '-', candidato)
-    numeros += 1
+risquinhos()
+centralizado('URNA ELETRONICA')
+risquinhos()
 
 while True:
+    for indice, candidato in enumerate(candidatos):
+        print(f'  {indice+1} - {candidato}')
 
-    while not (voto := input('\nDigite o número que deseja votar\nOu digite 0 para sair do programa: ')).isdigit() or (voto := int(voto)) < 0 or (voto := int(voto)) > 6:
-        print('Por favor, digite uma opção válida.')
+    risquinhos()
+
+    while not (voto := input(' Digite o número  que deseja votar\n Ou digite 0 para sair do programa: ')).isdigit() or (voto := int(voto)) < 0 or (voto := int(voto)) > 6:
+        print('\n Por favor, digite uma opção válida.')
+        risquinhos()
     
+    risquinhos()
+
     for pos in range(0, len(candidatos)):
         if voto == pos+1:
             votos[pos] += 1
     if voto == 0:
         break
 
-print('')
-
 for pos in range(0, len(candidatos)):
-    print(f'{candidatos[pos]} recebeu {votos[pos]} votos.')
+    print(f'  {candidatos[pos]} recebeu {votos[pos]} votos.')
+risquinhos()
