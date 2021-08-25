bc1359aca6031561f2083b76649f60282d71abf7
Luv <81397423+Luvlore@users.noreply.github.com>
2021-08-25 14:37:53 -0300

    Update atividade5_urna.py

--- atividade5_urna.py
+++ atividade5_urna.py
@@ -17,18 +17,20 @@ while True:
 
     risquinhos()
 
-    while not (voto := input(' Digite o número  que deseja votar\n Ou digite 0 para sair do programa: ')).isdigit() or (voto := int(voto)) < 0 or (voto := int(voto)) > 6:
+    while not (voto := input(' Digite o número  que deseja votar\n Ou digite 0 para sair do programa: ')).isdigit() or (voto := int(voto)) not in list(range(len(candidatos) + 1)):
         print('\n Por favor, digite uma opção válida.')
         risquinhos()
     
     risquinhos()
 
-    for pos in range(0, len(candidatos)):
+    for pos in range(len(candidatos)):
         if voto == pos+1:
             votos[pos] += 1
     if voto == 0:
         break
 
-for pos in range(0, len(candidatos)):
+for pos in range(len(candidatos) - 2):
     print(f'  {candidatos[pos]} recebeu {votos[pos]} votos.')
+risquinhos()
+print(f'  Votos {candidatos[4]}s: {votos[4]}\n  Votos {candidatos[5]}s: {votos[5]}')
 risquinhos()
